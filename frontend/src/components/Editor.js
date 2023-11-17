8a11b64457aff7c8597d3ada65d36f4d699a4021
torcuata22 <marilynjmarquez@gmail.com>
2023-11-17 12:56:03 -0500

    downgraded codemirror to codemirror5 and it is working now

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+// Import CodeMirror styles
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css"; // Adjust the theme as needed
 
-function Editor() {
-  return <div>Editor</div>;
+// Import CodeMirror mode files
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/css/css";
+
+function Editor(props) {
+  const { language, displayName, value, onChange } = props;
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+  return (
+    <div className="editor-contaienr">
+      <div className="editor-title">
+        {displayName}
+        <button>O/C</button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+        }}
+      />
+    </div>
+  );
 }
 
 export default Editor;
